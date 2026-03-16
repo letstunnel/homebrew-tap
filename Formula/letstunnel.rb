5607e563d1b63d72a8742f7cd05a0caef00df193
phinett <phinett10@gmail.com>
2026-03-16 16:08:30 +0000

    Update to v0.1.17

--- Formula/letstunnel.rb
+++ Formula/letstunnel.rb
@@ -8,22 +8,22 @@ class Letstunnel < Formula
   desc "Expose your localhost to the world — public URLs, .local domains, request inspector"
   homepage "https://letstunnel.com"
   license "MIT"
-  version "0.1.16"
+  version "0.1.17"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/letstunnel/letstunnel-releases/releases/download/v#{version}/lt-#{version}-darwin-arm64.tar.gz"
-      sha256 "9b63c4c53190239044dde8403f76afb5e0c6e3a9d096cb495e42f6710cf9dfe7"
+      sha256 "fe8342fd2a8d6a6deebe8cc26732eb3c30e54817437ceae699b2d288944fa4d2"
     elsif Hardware::CPU.intel?
       url "https://github.com/letstunnel/letstunnel-releases/releases/download/v#{version}/lt-#{version}-darwin-x64.tar.gz"
-      sha256 "95a75aecade26a8ce90ac2e991a149c3fedbcbb0ff4c763a0b65ecad9e79bdbe"
+      sha256 "307f679c0c738ca67f56bf25d5e67d32af9db38751cfc2c77859589f06a22836"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/letstunnel/letstunnel-releases/releases/download/v#{version}/lt-#{version}-linux-x64.tar.gz"
-      sha256 "6b1d7f8f83ad7351d25db205ef40cbef1c9489131c6088fb5ac6945cc93d3cb7"
+      sha256 "a24168ef968d0997b0cb0d304d23b10eec2842c1c5b79bda8a7bbdbe57453fc1"
     end
   end
 
